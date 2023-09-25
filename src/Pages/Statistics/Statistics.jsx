743bfa8ac20150ca528f2f48ca288bebc38b09b5
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-09-26 00:49:20 +0600

    pie chart completed

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import { Pie, PieChart, Tooltip } from "recharts";
+
 const Statistics = () => {
+    const [donatedNumber, setDonatedNumber] = useState([]);
+    useEffect(() => {
+        const totalDonation = JSON.parse(localStorage.getItem("donation"));
+        setDonatedNumber(totalDonation);
+    }, []);
+
+    const totalDonationLength = donatedNumber ? donatedNumber.length : 0;
+    const totalCards = 12;
+    const myDonation =  ((totalDonationLength / totalCards) * 100).toFixed(2) ;
+    const yourDonation = parseFloat(myDonation)
+    const totalDonation = (100 - yourDonation);
+
+    const data = [
+        { name: "Your Donation", value: yourDonation, fill: "#0088FE" },
+        { name: "Total Donation", value: totalDonation, fill: "#FFBB28" }
+    ];
+    const Value = data.map(values => values.value)
+    console.log(Value);
+
     return (
         <div>
-            <h2>This is statistics Page</h2>
+            <PieChart width={930} height={750}>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={200}
+                >
+
+                </Pie>
+                <Tooltip />
+            </PieChart>
+
         </div>
     );
 };
